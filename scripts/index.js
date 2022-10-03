349ca039aa78a91063d28e0eb8eec00554a4d26d
Alejo Lang <alejolang123@gmail.com>
2022-10-03 17:03:37 -0300

    Added an Ad

--- scripts/index.js
+++ scripts/index.js
@@ -21,3 +21,18 @@ const displayWeatherData = async () => {
 }
 
 window.addEventListener('load', displayWeatherData(), false)
+
+let adsImgs = [
+  'https://i.ytimg.com/vi/MaFRE1-sn5Q/maxresdefault.jpg',
+  'https://pbs.twimg.com/media/FJFdPCIXoAQh4yX.jpg',
+  'https://crehana-blog.imgix.net/media/filer_public/90/83/9083027a-fc03-4e3f-8f55-636ffce6d36c/mcdonalds-happy-meal.jpg?auto=format&q=50',
+  'https://quizizz.com/_media/quizzes/c2997833-61e0-4c05-bb77-955299282285_900_900',
+  'https://2.bp.blogspot.com/-_x0FdMPtsYI/TgLbdWiJs9I/AAAAAAAABFg/AiHZAdU_6zY/s1600/Halls1.jpg'
+]
+
+if(Math.random() > 0.4){
+  document.querySelector('.ad-img').src = adsImgs[Math.ceil(Math.random() * 4)]
+  document.querySelector('.ad-dialog').showModal()
+}
+
+document.querySelector('.close-ad').addEventListener('click', () => {document.querySelector('.ad-dialog').close()})
