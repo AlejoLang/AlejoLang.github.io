56abb034811213db95691401912f3dcd122a3e21
Alejo Lang <alejolang123@gmail.com>
2022-10-13 00:19:10 -0300

    Changed the WeatherAPI to AccuWeatherAPI

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,27 @@
 const getWeatherData = async () => {
-  const API_KEY = 'c1f12ce4caf4465abec190914222609';
-  const URL = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=auto:ip`;
+  const API_KEY = 'GdOtyPaUGVSU0iPE0jxpqSg1D4y7khQV';
+  const LOCATION = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/ipaddress?apikey=${API_KEY}&language=es-AR`)
+                    .then(res => res.json());
+  const URL = `http://dataservice.accuweather.com/currentconditions/v1/${LOCATION.Key}?apikey=${API_KEY}&language=es-AR&details=true`;
 
-  let data = await fetch(URL)
+  const data = await fetch(URL)
   if(!data.ok){
     alert('Error en el servidor')
   }
-  return data.json()
+
+  return {data: (await data.json())[0], location: LOCATION.LocalizedName}
 }
 
 const displayWeatherData = async () => {
-  const {location, current} = await getWeatherData();
+  const {location, data} = await getWeatherData();
+console.log(data.Wind, location)
 
-  document.querySelector('.time-title').textContent = `El tiempo en ${location.name}${location.region ? ', ' + location.region : ''} es: `
-  document.querySelector('.time-cond-img').src = current.condition.icon
-  document.querySelector('.time-cond-temp').textContent = `${current.temp_c}º C`
-  document.querySelector('.time-extra-feelslike').innerHTML = `Sensacion Térmica: <span>${current.feelslike_c}º C</span>`
-  document.querySelector('.time-extra-humidity').innerHTML = `Humedad: <span>${current.humidity}%</span>`
-  document.querySelector('.time-extra-wind').innerHTML = `Velocidad del viento: <span>${current.wind_kph} KM/h</span>`
+  document.querySelector('.time-title').textContent = `El tiempo en ${location} es: `
+  document.querySelector('.time-cond-img').src = `./images/icons/${data.WeatherIcon}.svg`
+  document.querySelector('.time-cond-temp').textContent = `${data.Temperature.Metric.Value}º C`
+  document.querySelector('.time-extra-feelslike').innerHTML = `Sensacion Térmica: <span>${data.RealFeelTemperature.Metric.Value}º C</span>`
+  document.querySelector('.time-extra-humidity').innerHTML = `Humedad: <span>${data.RelativeHumidity ?? '0'}%</span>`
+  document.querySelector('.time-extra-wind').innerHTML = `Velocidad del viento: <span>${data.Wind.Speed.Metric.Value} KM/h</span>`
 }
 
 window.addEventListener('load', displayWeatherData(), false)
